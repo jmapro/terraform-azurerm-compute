1594cb645427df2daaf739443235ae21a7d61350
Jeremy MARMOL <j.marmol@linkbynet.com>
2017-10-23 13:14:03 +0200

    Fix bootdiag issue

--- main.tf
+++ main.tf
@@ -135,10 +135,6 @@ resource "azurerm_virtual_machine" "vm-linux-with-datadisk" {
 
   tags = "${var.tags}"
 
-  boot_diagnostics {
-    enabled = "${var.boot_diagnostics}"
-    storage_uri = "${var.boot_diagnostics == "true" ? join(",", azurerm_storage_account.vm-sa.*.primary_blob_endpoint) : "" }"
-  }
 }
 
 resource "azurerm_virtual_machine" "vm-windows" {
@@ -182,11 +178,7 @@ resource "azurerm_virtual_machine" "vm-windows" {
   tags = "${var.tags}"
 
   os_profile_windows_config {}
-
-  boot_diagnostics {
-    enabled = "${var.boot_diagnostics}"
-    storage_uri = "${var.boot_diagnostics == "true" ? join(",", azurerm_storage_account.vm-sa.*.primary_blob_endpoint) : "" }"
-  }
+  
 
 }
 
